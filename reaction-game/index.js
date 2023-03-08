61ef03349d2ed590f0f0300ad28b12d770a2ccaf
hanameee <hanameee@gmail.com>
2023-03-09 01:11:00 +0900

    feat: [reaction-game] add waiting function (random time) & label

--- reaction-game/index.js
+++ reaction-game/index.js
@@ -4,6 +4,12 @@ const BUTTON_STATE_ENUM = {
     READY: "READY"
 }
 
+const BUTTON_STATE_LABEL = {
+    [BUTTON_STATE_ENUM.START]: "START GAME",
+    [BUTTON_STATE_ENUM.IN_PROGRESS]: "WAIT FOR IT...",
+    [BUTTON_STATE_ENUM.READY]: "CLICK NOW!!!"
+}
+
 const DEFAULT_BUTTON_STYLE = "text-white font-bold py-2 px-4 border-b-4 rounded"
 const STATE_BUTTON_STYLE = {
     [BUTTON_STATE_ENUM.START]: "bg-blue-500 hover:bg-blue-400 border-blue-700 hover:border-blue-500",
@@ -22,11 +28,16 @@ class Button {
         switch (this.state) {
             case BUTTON_STATE_ENUM.START:
                 this.state = BUTTON_STATE_ENUM.IN_PROGRESS
+                const randomTime = Math.random() * 10
+                console.info(`Button will change after ${randomTime.toFixed(2)}sec`)
+                setTimeout(() => {
+                    this.state = BUTTON_STATE_ENUM.READY;
+                    this.render();
+                }, randomTime * 1000)
                 this.render()
                 break
             case BUTTON_STATE_ENUM.IN_PROGRESS:
-                this.state = BUTTON_STATE_ENUM.READY;
-                this.render()
+                alert("Too Soon! 😉")
                 break
             case BUTTON_STATE_ENUM.READY:
                 this.state = BUTTON_STATE_ENUM.START;
@@ -41,7 +52,7 @@ class Button {
         const button = document.createElement("button");
         button.className = DEFAULT_BUTTON_STYLE + " " + STATE_BUTTON_STYLE[this.state];
         button.id = "button"
-        button.innerText = BUTTON_STATE_ENUM[this.state]
+        button.innerText = BUTTON_STATE_LABEL[this.state]
         button.addEventListener("click", () => this.changeButtonState())
 
         this.target.appendChild(button)
