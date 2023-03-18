32256f2a5375c0fb6f20b0a8c57ed54e493a2f62
hanameee <hanameee@gmail.com>
2023-03-18 16:22:58 +0900

    feat: [breakout-game] add collision detection function (bounce off the wall)
    * check if position after transition exceeds the boundary of a canvas
    * ball changes direction if it meets the wall
    * ball radius is taken into account when detecting boundary (to prevent ball from disappearing into wall)
    * ball color changes everytime it hits the wall

--- breakout-game/index.js
+++ breakout-game/index.js
@@ -3,16 +3,22 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
+const BALL_RADIUS = 10;
+let BALL_COLOR = "#0095DD";
+let X = canvas.width / 2;
+let Y = canvas.height - 30;
 
 let dx = 2;
 let dy = -2;
 
+function getRandomColor() {
+    return `#${Math.round(Math.random() * 0xffffff).toString(16)}`;
+}
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(X, Y, BALL_RADIUS, 0, Math.PI * 2);
+    ctx.fillStyle = BALL_COLOR;
     ctx.fill();
     ctx.closePath();
 }
@@ -24,8 +30,18 @@ function resetBall() {
 function draw() {
     resetBall();
     drawBall();
-    x += dx;
-    y += dy;
+    const NEW_X = X + dx;
+    const NEW_Y = Y + dy;
+    if (NEW_X < BALL_RADIUS || NEW_X > canvas.width - BALL_RADIUS) {
+        dx = -dx;
+        BALL_COLOR = getRandomColor();
+    }
+    if (NEW_Y < BALL_RADIUS || NEW_Y > canvas.height - BALL_RADIUS) {
+        dy = -dy;
+        BALL_COLOR = getRandomColor();
+    }
+    X += dx;
+    Y += dy;
 }
 
 setInterval(draw, 10);
